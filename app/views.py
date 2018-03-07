7d4f047649bf98e24013654ef6df42861a5b07e8
salima <bsalima010@gmail.com>
2018-03-07 19:17:03 +0100

    commit with master

--- app/views.py
+++ app/views.py
@@ -14,6 +14,27 @@ def topic_add():
         new_post = models.Post(request.form["title"], request.form["content"])
         post_store.add(new_post)
         return redirect(url_for("home"))
-
     else:
-        return render_template("topic_add.html")
+         return render_template("topic_add.html")
+
+@app.route("/topic/delete/<int:id>")
+def topic_delete(id):
+    post_store.delete(id)
+    return redirect(url_for("home"))
+
+@app.route("/topic/show/<int:id>")
+def topic_show(id):
+    return render_template("show_posts.html" ,post = post_store.get_by_id(id) )
+
+@app.route("/topic/update/<int:id>" , methods = ["GET", "POST"] )
+def topic_update(id):
+    if request.method == "POST":
+        post = post_store.get_by_id(id)
+        post.title = request.form["title"]
+        post.topic = request.form["content"]
+        post_store.update(post)
+        return redirect(url_for("home"))
+    else:
+        post = post_store.get_by_id(id)
+        return render_template("update_post.html", post=post)
+
