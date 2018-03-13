71556a1a7bb2f6077e7766c35c0c70b9e2282494
Unknown <bsalima010@gmail.com>
2018-03-13 10:46:45 +0100

    fix view

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, abort
 from app import models
 from app import app, post_store
 
@@ -19,24 +19,36 @@ def topic_add():
 
 @app.route("/topic/delete/<int:id>")
 def topic_delete(id):
-    post_store.delete(id)
+    try:
+        post_store.delete(id)
+    except ValueError:
+        abort(404)
     return redirect(url_for("home"))
 
 @app.route("/topic/show/<int:id>")
 def topic_show(id):
-    return render_template("show_posts.html" ,post = post_store.get_by_id(id) )
+    post = post_store.get_by_id(id)
+    if post is None:
+        abort(404, "Couldn't find this topic id !")
+    return render_template("show_posts.html" ,post = post )
 
 @app.route("/topic/update/<int:id>" , methods = ["GET", "POST"] )
 def topic_update(id):
+    post = post_store.get_by_id(id)
+    if post is None:
+        abort(404)
+
     if request.method == "POST":
-        post = post_store.get_by_id(id)
         post.title = request.form["title"]
         post.topic = request.form["content"]
         post_store.update(post)
         return redirect(url_for("home"))
     else:
-        post = post_store.get_by_id(id)
         return render_template("update_post.html", post=post)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', message = error.description)
 
 
 
