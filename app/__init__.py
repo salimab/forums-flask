5f5f1f170eebbbc02767e806a76f2edcc109cbdc
salima <bsalima010@gmail.com>
2018-03-21 13:40:40 +0100

    Add PostgreSQL db connection

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ import os
 app = Flask(__name__)
 
 folder_path = os.path.abspath(os.path.dirname(__file__))
-app.config["SQLALCHEMY_DATABASE_URI"] = """sqlite:///{0}""".format(os.path.join(folder_path, "my_database.db"))
-#app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
+#app.config["SQLALCHEMY_DATABASE_URI"] = """sqlite:///{0}""".format(os.path.join(folder_path, "my_database.db"))
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 
